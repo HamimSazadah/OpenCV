fba163e6ac6ccf39cda0d261f2fd626267a1a2be
HAMIM-PC\LENOVO <hamim.sazadah@gmail.com>
2017-10-06 10:56:27 +0700

    add K-Mean Clustering

---
+++ k_mean_clustering.py
@@ -0,0 +1,29 @@
+#import library numpy and OpenCV
+import numpy as np
+import cv2
+
+#load image
+img = cv2.imread('me-you.jpg')
+Z = img.reshape((-1,3))
+
+# Convertion to Float
+Z = np.float32(Z)
+
+# define criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+# numbers of cluster
+K = 8
+
+# run kmeans
+ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+# Now convert back into uint8, and make original image
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+
+#show image
+cv2.imshow('res2',res2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
